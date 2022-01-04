823fd14ed041d3e066237fee68f5e59219ee6333
Rafał Baran <70489677+rafal06@users.noreply.github.com>
2022-01-04 18:31:02 +0100

    Add zsh syntax highlighting plugin

--- install.sh
+++ install.sh
@@ -27,6 +27,9 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 	echo 'Installing powerlevel10k'
 	git clone --depth=1  https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
+
+	echo 'Installing zsh syntax highlighting'
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi;
 
 
